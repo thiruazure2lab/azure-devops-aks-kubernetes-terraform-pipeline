c451fe7af9fd19f9810c3b07db17b59b1ac0d4fa
thiruazure2lab <thiruazure2lab@gmail.com>
2024-07-05 09:48:49 +0530

    modified 2

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -5,52 +5,44 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   resource_group_name = azurerm_resource_group.aks_rg.name
   kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
+  role_based_access_control_enabled = true
+  http_application_routing_enabled =  true
+  azure_policy_enabled = true
+  cost_analysis_enabled = true
 
-
-  default_node_pool {
-    name       = "systempool"
-    vm_size    = "Standard_DS2_v3"
-    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    availability_zones   = [1, 2, 3]
-    enable_auto_scaling  = true
-    max_count            = 3
-    min_count            = 1
-    os_disk_size_gb      = 30
-    type           = "VirtualMachineScaleSets"
-    node_labels = {
-      "nodepool-type" = "system"
-      "environment"   = var.environment
-      "nodepoolos"    = "linux"
-      "app"           = "system-apps"
-    }
-    tags = {
-      "nodepool-type" = "system"
-      "environment"   = var.environment
-      "nodepoolos"    = "linux"
-      "app"           = "system-apps"
-    }    
+  azure_active_directory_role_based_access_control {
+    managed = true
+    admin_group_object_ids = azuread_group.aks_administrators
   }
-
+  oms_agent {
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
+    }
 # Identity (System Assigned or Service Principal)
   identity { type = "SystemAssigned" }
 
 # Add On Profiles
-  addon_profile {
-    azure_policy { enabled = true }
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
+  default_node_pool {
+      name       = "systempool"
+      vm_size    = "Standard_DS2_v3"
+      orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
+      enable_auto_scaling  = true
+      max_count            = 3
+      min_count            = 1
+      os_disk_size_gb      = 30
+      type           = "VirtualMachineScaleSets"
+      node_labels = {
+        "nodepool-type" = "system"
+        "environment"   = var.environment
+        "nodepoolos"    = "linux"
+        "app"           = "system-apps"
+      }
+      tags = {
+        "nodepool-type" = "system"
+        "environment"   = var.environment
+        "nodepoolos"    = "linux"
+        "app"           = "system-apps"
+      }    
     }
-  }
-
-# RBAC and Azure AD Integration Block
-role_based_access_control {
-  enabled = true
-  azure_active_directory {
-    managed                = true
-    admin_group_object_ids = [azuread_group.aks_administrators.id]
-  }
-}  
 
 # Windows Admin Profile
 windows_profile {
@@ -76,6 +68,7 @@ network_profile {
 tags = {
   Environment = var.environment
 }
+
 
 
 }
