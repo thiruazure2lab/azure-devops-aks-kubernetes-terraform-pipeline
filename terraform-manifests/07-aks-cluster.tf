7b2d688a48659512dac2464c65e5803ebe86c09a
thiruazure2lab <thiruazure2lab@gmail.com>
2024-07-05 12:01:12 +0530

    modifiedd 14

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -36,7 +36,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
       min_count            = 1
       os_disk_size_gb      = 30
       type           = "VirtualMachineScaleSets"
-      temporary_name_for_rotation = random_pet.aksrandom
+      temporary_name_for_rotation = "thiru_random"
       node_labels = {
         "nodepool-type" = "system"
         "environment"   = var.environment
