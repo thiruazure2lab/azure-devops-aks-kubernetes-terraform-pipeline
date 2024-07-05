8b3b91a6487dee02de1a74cd04c836933bbbfa0c
thiruazure2lab <thiruazure2lab@gmail.com>
2024-07-05 11:45:15 +0530

    modified ds3 to ds2

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -28,7 +28,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
   default_node_pool {
       name       = "systempool"
-      vm_size    = "Standard_DS3_v2"
+      vm_size    = "Standard_DS2_v2"
       orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
       enable_auto_scaling  = true
       max_count            = 3
